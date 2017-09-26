8d2c84f299c3fadc3f83c05ba22b529ea85fee37
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-25 23:01:09 -0500

    Agregando gulp y bower

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,6 +5,7 @@ class Login extends CI_Controller
 {
 	public function index()
 	{
+		
 		$this->load->view('login/index');
 	}
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,1 +1,13 @@
-<h1>Login</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="shortcut icon" href="<?php echo STATIC_URL;?>favicon.ico">
+	<link rel="stylesheet" type="text/css" href="<?php echo STATIC_URL;?>dist/styles.min.css">
+</head>
+<body>
+	<h1>login</h1>
+
+	<script type="text/javascript" src="<?php echo STATIC_URL;?>dist/libs.min.js"></script>
+</body>
+</html>
